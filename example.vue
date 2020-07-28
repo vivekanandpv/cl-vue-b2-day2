a12357db6a1820208d9a15b0146065ead2176576
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-28 23:51:22 +0530

    done

--- example.vue
+++ example.vue
@@ -2,7 +2,10 @@
   <div>
     <h4>Hi there!</h4>
     <hr />
-    <my-sample name="Ajay" city="Mumbai" country="India"></my-sample>
+    <div v-for="(p, i) in persons" :key="i">
+      <my-sample :name="p.fullName" :city="p.city" :country="p.country"></my-sample>
+      <hr />
+    </div>
   </div>
 </template>
 
@@ -10,6 +13,27 @@
 import Sample from "./sample";
 
 export default {
+  data: function () {
+    return {
+      persons: [
+        {
+          fullName: "Ajay Kumar",
+          city: "Bengaluru",
+          country: "India",
+        },
+        {
+          fullName: "John Miller",
+          city: "London",
+          country: "UK",
+        },
+        {
+          fullName: "Roger Godwin",
+          city: "Toronto",
+          country: "Canada",
+        },
+      ],
+    };
+  },
   components: {
     "my-sample": Sample,
   },
