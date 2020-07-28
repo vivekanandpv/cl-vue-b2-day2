efca072605a015cdccc51e94b795ee0a0844a2a3
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-29 00:09:34 +0530

    done

--- example.vue
+++ example.vue
@@ -2,23 +2,7 @@
   <div>
     <h4>Hi there!</h4>
     <hr />
-    <my-sample :name="fullName">
-      <!-- v-slot directive can only be applied to template -->
-      <template v-slot:s2>
-        <h4>Slot: s2</h4>
-      </template>
-
-      <template v-slot:s1>
-        <h4>Slot: s1</h4>
-      </template>
-
-      <!-- with default slot explicitly defined, will this show? -->
-      <!-- <h4>Default slot</h4> -->
-
-      <template v-slot:default>
-        <h4>Slot: Default, explicit</h4>
-      </template>
-    </my-sample>
+    <my-sample :name="fullName" @custom-event="parentHandler"></my-sample>
   </div>
 </template>
 
@@ -30,6 +14,11 @@ export default {
     return {
       fullName: "Ajay Kishore",
     };
+  },
+  methods: {
+    parentHandler: function (...args) {
+      console.log("Example handler called", args);
+    },
   },
   components: {
     "my-sample": Sample,
--- sample.vue
+++ sample.vue
@@ -1,18 +1,17 @@
 <template>
   <div>
-    <p>Name: {{name}}</p>
-    <slot name="s1"></slot>
-    <p>------------------</p>
-    <slot name="s2"></slot>
-    <p>------------------</p>
-    <slot></slot>
-    <hr />
+    <button @click="clickHandler">Click me to emit custom event</button>
   </div>
 </template>
 
 <script>
 export default {
   props: ["name"],
+  methods: {
+    clickHandler: function () {
+      this.$emit("custom-event", 100, "Hi there!", { name: "Mohan" });
+    },
+  },
 };
 </script>
 
