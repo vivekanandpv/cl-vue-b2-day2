8de252fcae85162d5f0f1eea051b6134ee06c111
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-28 23:42:48 +0530

    done

--- example.vue
+++ example.vue
@@ -1,51 +1,17 @@
 <template>
   <div>
-    <h3>{{counter}}</h3>
-    <button @click="clickHandler">Increment</button>
+    <h4>Hi there!</h4>
+    <hr />
+    <my-sample></my-sample>
   </div>
 </template>
 
 <script>
+import Sample from "./sample";
+
 export default {
-  data: function () {
-    return {
-      counter: 0,
-    };
-  },
-  methods: {
-    clickHandler: function () {
-      ++this.counter;
-    },
-  },
-  beforeCreate: function () {
-    //  reactivity is not yet in place
-    console.log("Before Create", this.counter);
-  },
-  created: function () {
-    //  data, such as counter is reactive now
-    console.log("Created", this.counter);
-  },
-  beforeMount: function () {
-    //  template is compiled and v-dom is ready
-    //  no data-binding, {{counter}} is plain string now
-    //  eq: ngOnInit
-    console.log("Before Mount");
-  },
-  mounted: function () {
-    //  data binding completed
-    //  native dom is hooked
-    //  eq: ngAfterViewInit
-    //  use for external data-communication
-    console.log("Mounted");
-  },
-  beforeUpdate: function () {
-    //  after update in the component
-    //  before DOM node is re-rendered
-    console.log("Before Update");
-  },
-  updated: function () {
-    //  after DOM node is re-rendered
-    console.log("Updated");
+  components: {
+    "my-sample": Sample,
   },
 };
 </script>
---
+++ sample.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>{{title}}</p>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      title: "I am sample component",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
