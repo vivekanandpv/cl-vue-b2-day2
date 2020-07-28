b42da814a1264e5f1806747aa02b4db79436a059
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-28 23:22:21 +0530

    done

--- example.vue
+++ example.vue
@@ -1,29 +1,22 @@
 <template>
   <div>
-    <!-- Fired only once -->
-    <button @click.once="clickHandler('once')">Click (once)</button>
-    <hr />
-    <!-- No page reload; useful for form submission -->
-    <button @click.prevent="clickHandler('prevent')">Click (prevent)</button>
-    <hr />
-    <!-- Child events are not considered -->
-    <div @click.self="clickHandler('self')" class="card">
-      <button>Click (self-at-outer)</button>
-    </div>
-    <hr />
-    <!-- Child events are prioritized for parent -->
-    <div @click.capture="clickHandler('capture outer')" class="card">
-      <button @click="clickHandler('capture inner')">Click (capture-out-then-in)</button>
+    <div v-for="(value, name, index) in person" :key="index">
+      <p>{{index}}. {{name}} -> {{value}}</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  methods: {
-    clickHandler: function (args) {
-      console.log("Clicked", args);
-    },
+  data: function () {
+    return {
+      person: {
+        name: "Vijay",
+        city: "Bengaluru",
+        country: "India",
+        empCode: 12345,
+      },
+    };
   },
 };
 </script>
