6d7e111cb8b245baf2ca2a857bc3b976b4140e02
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-28 23:30:01 +0530

    done

--- example.vue
+++ example.vue
@@ -1,25 +1,71 @@
 <template>
   <div>
-    <ul v-for="n in 10" :key="n">
-      <li>Current value: {{n}}</li>
-    </ul>
+    <h2>{{name}}</h2>
+    <p>{{city}}</p>
+    <hr />
+    <p :class="style">This is a sample text with dynamic styling</p>
+    <button @click="changeStyle">Change Style</button>
   </div>
 </template>
 
 <script>
 export default {
-  data: function () {
+  name: "sample",
+  data() {
     return {
-      person: {
-        name: "Vijay",
-        city: "Bengaluru",
-        country: "India",
-        empCode: 12345,
-      },
+      name: "Vinayak",
+      city: "Hyderabad",
+      style: null,
+      index: 0,
+      styleArray: [
+        {
+          active: true,
+          paid: false,
+        },
+        {
+          active: false,
+          paid: true,
+        },
+        {
+          active: true,
+          paid: true,
+        },
+      ],
     };
+  },
+  methods: {
+    changeStyle() {
+      ++this.index;
+      if (this.index === 3) {
+        this.index = 0;
+      }
+
+      this.style = this.styleArray[this.index];
+    },
   },
 };
 </script>
 
-<style>
+<style scoped>
+h2 {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 30px;
+  color: blue;
+}
+
+p {
+  font-size: 14px;
+  color: brown;
+}
+
+.active {
+  color: green;
+  font-size: 24px;
+  font-family: "Courier New", Courier, monospace;
+}
+
+.paid {
+  font-style: italic;
+  text-decoration: underline;
+}
 </style>
