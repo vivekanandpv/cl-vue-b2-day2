13bf3165a8247b7734f2bab329f130df6599b8e4
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-28 21:56:11 +0530

    done

--- example.vue
+++ example.vue
@@ -1,9 +1,29 @@
 <template>
-  <h2>Hi there!</h2>
+  <div>
+    <h2>Calculation of Simple Interest</h2>
+    <p>Principal: {{principal}}</p>
+    <p>Rate of interest: {{roi}} %</p>
+    <p>Term: {{term}} years</p>
+    <hr />
+    <p>Simple Interest: {{interest}}</p>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function () {
+    return {
+      principal: 10000,
+      roi: 7.5,
+      term: 5,
+    };
+  },
+  computed: {
+    interest: function () {
+      return (this.principal * this.roi * this.term) / 100;
+    },
+  },
+};
 </script>
 
 <style>
