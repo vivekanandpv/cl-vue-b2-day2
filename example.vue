0429b825d5fd2a546d23851876eaa97a90dc1db6
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-28 23:17:26 +0530

    done

--- example.vue
+++ example.vue
@@ -1,14 +1,28 @@
 <template>
   <div>
-    <button @click="clickHandler('Hi there!', 100, {year: 2005})">Click me!</button>
+    <!-- Fired only once -->
+    <button @click.once="clickHandler('once')">Click (once)</button>
+    <hr />
+    <!-- No page reload; useful for form submission -->
+    <button @click.prevent="clickHandler('prevent')">Click (prevent)</button>
+    <hr />
+    <!-- Child events are not considered -->
+    <div @click.self="clickHandler('self')" class="card">
+      <button>Click (self-at-outer)</button>
+    </div>
+    <hr />
+    <!-- Child events are prioritized for parent -->
+    <div @click.capture="clickHandler('capture outer')" class="card">
+      <button @click="clickHandler('capture inner')">Click (capture-out-then-in)</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   methods: {
-    clickHandler: function (...args) {
-      console.log(args);
+    clickHandler: function (args) {
+      console.log("Clicked", args);
     },
   },
 };
